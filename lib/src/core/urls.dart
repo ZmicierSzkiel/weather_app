0607d9d9cd4e95092c87650911b60a8782826c85
Zmicier Szkiel <zmicierszkiel@gmail.com>
2023-08-08 18:26:47 +0200

    add minor changes

--- lib/src/core/urls.dart
+++ lib/src/core/urls.dart
@@ -1,5 +1,13 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class Urls {
   static String weatherIcon(String iconCode) =>
       'http://openweathermap.org/img/wn/$iconCode@2x.png';
   static String baseUrl = 'api.openweathermap.org';
+  static String baseLatitude = '53.9006';
+  static String baseLongitude = '27.5590';
+  static Map<String, String> headers = {
+    'Content-Type': 'x-www-form-urlencoded',
+  };
+  static String token = dotenv.env['API_TOKEN'] ?? '';
 }
--- lib/src/data/provider/weather_repository_provider.dart
+++ lib/src/data/provider/weather_repository_provider.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
 import 'package:weather_app/src/core/urls.dart';
@@ -12,51 +11,69 @@ import 'package:weather_app/src/domain/entity/weather_api_response.dart';
 import 'package:weather_app/src/domain/entity/weather_data.dart';
 
 class WeatherRepositoryProvider {
-  final String token = dotenv.env['API_TOKEN'] ?? '';
-
   Future<WeatherApiResponse> getWeatherDataFromApi() async {
     try {
       final Position position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.high,
       );
-      final Map<String, String> headers = {
-        'Content-Type': 'x-www-form-urlencoded',
-      };
+
       final Uri uri = Uri.https(
         Urls.baseUrl,
         '/data/2.5/forecast',
         {
           'lat': position.latitude.toString(),
           'lon': position.longitude.toString(),
-          'appid': token,
+          'appid': Urls.token,
           'units': 'metric',
         },
       );
       final http.Response response = await http.get(
         uri,
-        headers: headers,
+        headers: Urls.headers,
       );
-      if (response.statusCode == 200) {
-        final List<dynamic> forecastData = jsonDecode(response.body)['list'];
-        final City cityData = CityModel.fromJson(
-          jsonDecode(response.body)['city'],
-        );
-        final List<WeatherData> weatherDataList = forecastData
-            .map(
-              (e) => WeatherDataModel.fromJson(e),
-            )
-            .toList();
-        final WeatherApiResponse weatherApiResponse = WeatherApiResponse(
-          weatherDataList: weatherDataList,
-          cityData: cityData,
-        );
-        return weatherApiResponse;
-      } else {
-        throw Exception('Failed to load weather data');
-      }
+      final List<dynamic> forecastData = jsonDecode(response.body)['list'];
+      final City cityData = CityModel.fromJson(
+        jsonDecode(response.body)['city'],
+      );
+      final List<WeatherData> weatherDataList = forecastData
+          .map(
+            (e) => WeatherDataModel.fromJson(e),
+          )
+          .toList();
+      final WeatherApiResponse weatherApiResponse = WeatherApiResponse(
+        weatherDataList: weatherDataList,
+        cityData: cityData,
+      );
+      return weatherApiResponse;
     } catch (e) {
-      print(e.toString());
-      throw Exception(e.toString());
+      final Uri uri = Uri.https(
+        Urls.baseUrl,
+        '/data/2.5/forecast',
+        {
+          'lat': Urls.baseLatitude,
+          'lon': Urls.baseLongitude,
+          'appid': Urls.token,
+          'units': 'metric',
+        },
+      );
+      final http.Response response = await http.get(
+        uri,
+        headers: Urls.headers,
+      );
+      final List<dynamic> forecastData = jsonDecode(response.body)['list'];
+      final City cityData = CityModel.fromJson(
+        jsonDecode(response.body)['city'],
+      );
+      final List<WeatherData> weatherDataList = forecastData
+          .map(
+            (e) => WeatherDataModel.fromJson(e),
+          )
+          .toList();
+      final WeatherApiResponse weatherApiResponse = WeatherApiResponse(
+        weatherDataList: weatherDataList,
+        cityData: cityData,
+      );
+      return weatherApiResponse;
     }
   }
 }
